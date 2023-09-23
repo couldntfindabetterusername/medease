b4a418a6145a5861c9e7ca31a2593f0b4bee25fa
couldntfindabetterusername <bt20ece098@iiitn.ac.in>
2023-09-23 16:42:38 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+// console.log(process.env);
+const PORT = process.env.PORT || 5000;
+
+//connecting database
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
